74e9b62e8faf7260122663c3b7be7fe4a3b72336
Bhoomika-R2 <bhoomika@careercarve.com>
2024-12-25 16:21:47 +0530

    updates

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,10 @@
+import Navigation from "../components/Navigation";
 export default function MapPage() {
     return (
       <div className="coming-soon">
         <h1>Map View Coming Soon</h1>
         <p>We're working on bringing you an interactive map experience!</p>
+        <Navigation />
       </div>
     );
   }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,10 @@
+import Navigation from "../components/Navigation";
 export default function ProfilePage() {
     return (
       <div className="coming-soon">
         <h1>Login Coming Soon</h1>
         <p>We're working on bringing you a seamless login experience!</p>
+        <Navigation />
       </div>
     );
   }
